941811fe1a860dd6b5542ac3098a465effa44022
AlexAriasL <alex.arias.l@gmail.com>
2020-03-29 18:45:42 +0100

    Updated URL for GitHub API

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -41,7 +41,7 @@ function fetchGitHubInformation(event) {
 
 
     $.when(
-        $.getJSON("https://api.github.com/users/${username}")
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
         function (response) {
             var userData = response;
